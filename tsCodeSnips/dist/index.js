29b563820b567652632235dc960ded50b21e7c82
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2025-11-16 00:33:06 +0530

    dist
    dist

---
+++ tsCodeSnips/dist/index.js
@@ -0,0 +1,5 @@
+function addNumbers(n, m) {
+    return n + m;
+}
+var result = addNumbers(34, 'five');
+console.log('Result:', result); // Result: 90
---
+++ tsCodeSnips/dist/index.ts
@@ -0,0 +1,5 @@
+function addNumbers(n: number, m: number): number {
+  return n + m;
+}
+const result = addNumbers(34, 'five');
+console.log('Result:', result); // Result: 90
