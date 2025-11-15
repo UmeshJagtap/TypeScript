70128f8f6eadb6a175e93209322dd26b44528e32
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2025-11-16 00:36:16 +0530

    L6 -- TS classes & interface : classes, access modifiers (public, private, readonly), inheritence
    L6 -- TS classes & interface : classes, access modifiers (public, private, readonly), inheritence

---
+++ tsCodeSnips/L6/analytics.ts
@@ -0,0 +1,2 @@
+console.log('This is analytics file');
+// This file is excluded in tsconfig.json
---
+++ tsCodeSnips/L6/interface.ts
@@ -0,0 +1,5 @@
+// Console log statement
+console.log('Lets learn classes and interface in TypeScript');
+console.log('This is interface - !!!!!12345');
+
+// ------------------------------------------------------ ( type unknown examples )
